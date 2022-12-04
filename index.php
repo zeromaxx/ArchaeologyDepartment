c6526125a565069238005e50c3f985a7bb7f42a9
Aris Lamprinidis <zeromixer2010@yahoo.com>
2022-12-04 14:34:28 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Department Of Archaeology</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./index.css">
+</head>
+
+<body>
+    <section>
+        <form action="./overview.php" method="POST">
+            <h2>Καταγραφή στοιχείων ευρήματος</h2>
+            <label for="category">Κατηγορία Ευρήματος</label>
+            <input required type="text" name="category">
+            <label for="title">Tίτλος Ευρήματος</label>
+            <input required type="text" name="title">
+            <label for="date">Εκτιμούμενη χρονολογία</label>
+            <input required type="datetime" name="date">
+            <label for="site">Αρχαιολογικός Χώρος</label>
+            <input required type="text" name="site">
+            <label for="description">Περιγραφή</label>
+            <textarea name="description" id="" cols="30" rows="10"></textarea>
+            <label for="height">Ύψος</label>
+            <input required type="number" name="height">
+            <label for="length">Μήκος</label>
+            <input required type="number" name="length">
+            <label for="name">Όνομα</label>
+            <input required type="text" name="name">
+            <label for="lastname">Επώνυμο</label>
+            <input required type="text" name="lastname">
+            <label for=" ephorate">Αρμόδια Εφορεία Αρχαιοτήτων</label>
+            <input required type="text" name="ephorate">
+            <input name="submit" class="submitButton" value="Καταχώρηση" type="submit"></input>
+        </form>
+        <h3>Όνομα φοιτητή</h3>
+        <div class="overlay"></div>
+    </section>
+
+
+</body>
+
+</html>
---
+++ overview.php
@@ -0,0 +1,81 @@
+<?php
+$servername = "localhost";
+$username = "root";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=departmentOfArchaeologyDB", $username);
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
+date_default_timezone_set('Europe/Athens');
+$currentDateTime = date('d-m-y h:i:s');
+
+$category = $_POST['category'];
+$title = $_POST['title'];
+$date = $_POST['date'];
+$site = $_POST['site'];
+$description = $_POST['description'];
+$height = $_POST['height'];
+$length = $_POST['length'];
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$ephorate = $_POST['ephorate'];
+
+
+$sql = "INSERT INTO findings (Category,Title,EstimateDate,Site,Description,Width,Length,Name,Lastname,Ephorates,EntryTime)
+            VALUES('$category','$title','$date','$site','$description','$height','$length','$name','$lastname','$ephorate','$currentDateTime ')";
+$statement = $conn->prepare($sql);
+$statement->execute();
+
+$query = "SELECT Id FROM findings WHERE Id = LAST_INSERT_ID()";
+$result = $conn->query($query);
+$getLastId = $result->fetchColumn();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Overview</title>
+    <link rel="stylesheet" href="./index.css">
+</head>
+
+<body>
+    <section>
+        <table>
+            <h1>Επισκόπηση Ευρήματος</h1>
+            <tr>
+                <th>Αριθμός Ευρήματος </th>
+                <th>Κατηγορία Ευρήματος </th>
+                <th>Τίτλος Ευρήματος</th>
+                <th>Εκτιμούμενη χρονολογία</th>
+                <th>Αρχαιολογικός Χώρος</th>
+                <th>Περιγραφή</th>
+                <th>Εμβαδόν</th>
+                <th>Όνομα</th>
+                <th>Επώνυμο</th>
+                <th>Ώρα καταχώρησης</th>
+            </tr>
+            <tr>
+                <td><?php echo $getLastId ?></td>
+                <td><?php echo $category  ?></td>
+                <td><?php echo $title  ?></td>
+                <td><?php echo $date  ?></td>
+                <td><?php echo $site  ?></td>
+                <td><?php echo $description  ?></td>
+                <td><?php echo ($height * $length) ?></td>
+                <td><?php echo $name  ?></td>
+                <td><?php echo $lastname  ?></td>
+                <td><?php echo $currentDateTime ?></td>
+            </tr>
+        </table>
+        <a href="./index.php">Πίσω στην Αρχική</a>
+        <div class="overlay"></div>
+    </section>
+    <h3>Όνομα φοιτητή</h3>
+</body>
+
+</html>
